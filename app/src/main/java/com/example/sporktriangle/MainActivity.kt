b62a06a5e1fbfa220891ec369bcf213067b888a6
Eric <beatjunkieeric@gmail.com>
2021-09-22 10:49:09 -0700

    Created Toast message for verification purposes, App now checks if inputs are Null/Blank

--- app/src/main/java/com/example/sporktriangle/MainActivity.kt
+++ app/src/main/java/com/example/sporktriangle/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,34 +27,32 @@ class MainActivity : AppCompatActivity() {
         button = findViewById(R.id.goButton)
 
 //       Takes in user input values on button click and turns them into integers
-        button.setOnClickListener{
-            var length1 = inputLength1.text.toString().toInt()
-            var length2 = inputLength2.text.toString().toInt()
-            var length3 = inputLength3.text.toString().toInt()
+        button.setOnClickListener {
+            if ((!inputLength1.text.isNullOrBlank())&&(!inputLength2.text.isNullOrBlank())&&(!inputLength3.text.isNullOrBlank())) {
+                var length1 = inputLength1.text.toString().toInt()
+                var length2 = inputLength2.text.toString().toInt()
+                var length3 = inputLength3.text.toString().toInt()
 
-//          Variables to set minium and maximum range that app will accept.
-            var minRange = 1;
-            var maxRange = 100;
+//          Variables to set minimum and maximum range that app will accept.
+                var minRange = 1;
+                var maxRange = 100;
 
 //          Form validation for when user inputs something that is not within the range of 1-100
                 if (length1 < minRange || length1 > maxRange) {
-                    inputLength1.setError("Please enter a number between 1-100");
-                    inputLength1.requestFocus();
+                    Toast.makeText(this, "First Length should range from 1- 100", Toast.LENGTH_SHORT).show()
+                } else if (length2 < minRange || length2 > maxRange) {
+                    Toast.makeText(this, "Second Length should range from 1- 100", Toast.LENGTH_SHORT).show()
+                } else if (length3 < minRange || length3 > 100) {
+                    Toast.makeText(this, "Third Length should range from 1- 100", Toast.LENGTH_SHORT).show()
+
                 }
-                else if (length2 < minRange || length2 > maxRange) {
-                    inputLength2.setError("Please enter a number between 1-100");
-                    inputLength2.requestFocus();
-                }
-                else if (length3 < minRange || length3 > 100) {
-                    inputLength3.setError("Please enter a number between 1-100");
-                    inputLength3.requestFocus();
-                }
+
+            }
+                else{
+                Toast.makeText(this, "Fill in All inputs", Toast.LENGTH_LONG).show()
+            }
 
         }
-
-
-
     }
-
 
 }
