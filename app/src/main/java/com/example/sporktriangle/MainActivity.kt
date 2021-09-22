abc59d11877b4fd25bd40852440e940c2d5b2faa
Tiffany <8566039+Tiffufu@users.noreply.github.com>
2021-09-21 20:52:42 -0700

    Added form validation to input fields task
    Users will now get an error if they enter a number that is outside the range of 1-100

--- app/src/main/java/com/example/sporktriangle/MainActivity.kt
+++ app/src/main/java/com/example/sporktriangle/MainActivity.kt
@@ -7,10 +7,10 @@ import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
-
-    lateinit var editText: EditText
-    lateinit var editText2: EditText
-    lateinit var editText3: EditText
+//  Initialize variables for user to input text
+    lateinit var inputLength1: EditText
+    lateinit var inputLength2: EditText
+    lateinit var inputLength3: EditText
     lateinit var button: Button
 
 
@@ -18,24 +18,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        editText = findViewById(R.id.editTextNumber)
-        editText2 = findViewById(R.id.editTextNumber2)
-        editText3 = findViewById(R.id.editTextNumber3)
+//       Maps variables to editText/input fields
+        inputLength1 = findViewById(R.id.editTextNumber)
+        inputLength2 = findViewById(R.id.editTextNumber2)
+        inputLength3 = findViewById(R.id.editTextNumber3)
 
         button = findViewById(R.id.goButton)
 
+//       Takes in user input values on button click and turns them into integers
         button.setOnClickListener{
-            var length1 = editText.text.toString().toInt()
-            var length2 = editText2.text
-            var length3 = editText3.text
+            var length1 = inputLength1.text.toString().toInt()
+            var length2 = inputLength2.text.toString().toInt()
+            var length3 = inputLength3.text.toString().toInt()
 
-            if(length1 < 1 || length1 >100){
-                println("hello")
-            }
+//          Variables to set minium and maximum range that app will accept.
+            var minRange = 1;
+            var maxRange = 100;
+
+//          Form validation for when user inputs something that is not within the range of 1-100
+                if (length1 < minRange || length1 > maxRange) {
+                    inputLength1.setError("Please enter a number between 1-100");
+                    inputLength1.requestFocus();
+                }
+                else if (length2 < minRange || length2 > maxRange) {
+                    inputLength2.setError("Please enter a number between 1-100");
+                    inputLength2.requestFocus();
+                }
+                else if (length3 < minRange || length3 > 100) {
+                    inputLength3.setError("Please enter a number between 1-100");
+                    inputLength3.requestFocus();
+                }
 
         }
 
 
 
     }
+
+
 }
